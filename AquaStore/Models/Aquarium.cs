2465b38bea243114b11048f8b68f818a550e48fc
nepodeveloper <neponed@gmail.com>
2021-11-09 11:52:38 +0200

    Add project files.

---
+++ AquaStore/Models/Aquarium.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AquaStore.Models
+{
+    public class Aquarium
+    {
+        [Key]
+        public int AquariumlId { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string GlassType { get; set; }
+
+        public int Size { get; set; }
+
+        [Column(TypeName = "nvarchar(25)")]
+        public string Shape { get; set; }
+
+        //Foregien Key Constrain
+        public int FishlId { get; set; }
+        public Fish Fish { get; set; }
+
+    }
+}
---
+++ AquaStore/Models/Fish.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AquaStore.Models
+{
+    public class Fish
+    {
+        [Key]
+        public int FishlId { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Species { get; set; }
+
+        [Column(TypeName = "nvarchar(25)")]
+        public string Color { get; set; }
+
+        public int Fins { get; set; }
+
+
+
+    }
+}
